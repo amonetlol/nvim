1e480729d5a1679ccec96e6bf1a78c9c91b16865
amonetlol <65382640+amonetlol@users.noreply.github.com>
2026-06-27 18:58:22 -0300

    Update themes.lua

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -8,51 +8,62 @@ return {
   {
     "rebelot/kanagawa.nvim",
     priority = 900,
+    lazy = false,
   },
 
   {
     "vague-theme/vague.nvim",
     priority = 900,
+    lazy = false,
   },
 
   {
     "bluz71/vim-moonfly-colors",
     priority = 900,
+    lazy = false,
   },
 
   {
     "AlexvZyl/nordic.nvim",
     priority = 900,
+    lazy = false,
   },
 
   {
     "neanias/everforest-nvim",
     priority = 900,
+    lazy = false,
   },
 
   {
     "ellisonleao/gruvbox.nvim",
     priority = 900,
+    lazy = false,
   },
 
   {
     "shaunsingh/nord.nvim",
     priority = 900,
+    lazy = false,
   },
 
   {
     "Mofiqul/dracula.nvim",
     priority = 900,
+    lazy = false,
   },
 
   {
     "olimorris/onedarkpro.nvim",
     priority = 900,
+    lazy = false,
   },
 
   {
     "craftzdog/solarized-osaka.nvim",
+    name = "solarized",
     priority = 900,
+    lazy = false,
   },
 
   {
@@ -63,16 +74,19 @@ return {
   {
     "savq/melange-nvim",
     priority = 900,
+    lazy = false,
   },
 
   {
     "ribru17/bamboo.nvim",
     priority = 900,
+    lazy = false,
   },
 
   {
     "sainnhe/edge",
     priority = 900,
+    lazy = false,
   },
 
   {
